0f28e4f163b3371cc91ad1646a5e70a123f58804
anayafj <franciscojanaya80@gmail.com>
2021-11-28 22:46:04 -0800

    added typing

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,28 +3,51 @@ import styles from '../../styles/components/Header.module.scss';
 import classnames  from 'classnames';
 import Navigation from './Navigation';
 
-function Header() {
-    const [btnToggle, setBtnToggle] = useState(false);
+type ButtonText = 'Log Out'| 'Log In';
+type LoginButtonIcon = 'sign-out' | 'sign-in';
 
-    let toggleButton = classnames(
+// THINGS TO DO
+// 1. add useRef to track the login status.
+// 2. Adjust method to show navigaion only once logged in.
+// 3. Get links working.
+// 4. Work on login credentials (email/password, google).
+
+function Header() {
+    // state for Login toggle button
+    const [btnToggle, setBtnToggle] = useState<true | false>(false);
+    // state for Login status
+    const [loginStatus, setLoginStatus] = useState<true | false>(false);
+
+    // var for Login button classes
+    let toggleButton:string = classnames(
         'ui small button',
         `${styles.loginBtn}`,
         {'orange': !btnToggle}, 
         {'white basic': btnToggle}
         );
 
-    let btnText = btnToggle ? 'Log Out': 'Log In';
+    // var for Login button text
+    let btnText:ButtonText = btnToggle ? 'Log Out': 'Log In';
+    // var for Login button icon text
+    let loginButtonIcon:LoginButtonIcon = btnToggle ? 'sign-out': 'sign-in';
 
+    const loginButtonClicked = () => {
+        setBtnToggle(!btnToggle)
+    }
+
+    const setLoginMode = () => {
+        console.log("Login mode = ",loginStatus);
+    }
 
 	return (
         <header className={styles.header}>
             <div className={styles.logo}>LOGO</div>
             <div className={styles.controls}>
-            <button className={toggleButton} onClick={() => setBtnToggle(!btnToggle)}>
-                <i className={`${btnToggle ? 'sign-out': 'sign-in'} icon`}></i>
-                <span>{btnText}</span>
+                <button className={toggleButton} onClick={() => loginButtonClicked()}>
+                    <i className={`${loginButtonIcon} icon`}></i>
+                    <span>{btnText}</span>
                 </button>	
-			<Navigation />
+			    <Navigation />
             </div>
 		
          </header>
